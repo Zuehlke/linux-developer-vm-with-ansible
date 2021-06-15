ccfd15b97cf93809336177e769e9a1e4c424a7d7
Torben Knerr <mail@tknerr.de>
2021-06-15 22:54:59 +0200

    manage git alias and git config keys via ansible

--- spec/test_git.py
+++ spec/test_git.py
@@ -1,3 +1,4 @@
+import pytest
 import os
 
 def test_git_package_is_installed_(host):
@@ -16,3 +17,26 @@ def test_git_shell_prompt_is_configured_in_bashrc_d_(host):
 
 def test_git_shell_prompt_is_set_in_the_environment_(host):
     assert '__git_ps1' in host.run('bash -i -c "env | grep ^PS1=; exit \\$?"').stdout
+
+
+def test_gitconfig_configures_rebase_on_pull_(host):
+    __assert_git_config(host, 'pull.rebase', 'true')
+
+def test_gitconfig_configures_autocrlf_input_(host):
+    __assert_git_config(host, 'core.autocrlf', 'input')
+
+@pytest.mark.parametrize('shortcut,command', [
+    ('co', 'checkout'),
+    ('ci', 'commit'),
+    ('br', 'branch'),
+    ('st', 'status'),
+    ('unstage', 'reset HEAD --'),
+    ('slog', 'log --pretty=oneline --abbrev-commit'),
+    ('graph', 'log --all --oneline --graph --decorate')
+])
+def test_gitconfig_provides_alias_(host, shortcut, command):
+    __assert_git_config(host, f"alias.{shortcut}", command)
+
+def __assert_git_config(host, config_key, expected_value):
+    actual_value = host.run(f"git config --global --get {config_key}").stdout.strip()
+    assert actual_value == expected_value
