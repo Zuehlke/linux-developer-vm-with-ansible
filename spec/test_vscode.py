87bfd386a59b8ac486e2d4acc830998c700dd284
Torben Knerr <mail@tknerr.de>
2021-06-15 23:28:26 +0200

    install zbr.vscode-ansible plugin

--- spec/test_vscode.py
+++ spec/test_vscode.py
@@ -1,3 +1,4 @@
+import pytest
 
 def test_vscode_apt_sources_list_exists_(host):
     assert host.file('/etc/apt/sources.list.d/packages_microsoft_com_repos_vscode.list').exists
@@ -10,3 +11,10 @@ def test_vscode_command_is_found_(host):
 
 def test_vscode_version_command_succeeds_(host):
     assert host.run('code --version').rc is 0
+
+@pytest.mark.parametrize('extension', [
+    'zbr.vscode-ansible'
+])
+def test_vscode_extensions_include_defined_extensions_(host, extension):
+    installed_extensions = host.run('code --list-extensions').stdout
+    assert extension in installed_extensions
