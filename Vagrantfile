tknerr/yocto-developer-vm

switch to generic ubuntu basebox and install the desktop packages manually

Vagrantfile
@@ -2,14 +2,15 @@
 Vagrant.configure("2") do |config|
 
   # basebox
-  config.vm.box = 'tknerr/ubuntu2004-desktop'
-  config.vm.box_version = '1.0.0'
+  config.vm.box = 'generic/ubuntu2004'
+  config.vm.box_version = '3.1.6'
 
   # hostname
   config.vm.hostname = 'yocto-dev-vm'
 
   # virtualbox specific customizations
   config.vm.provider "virtualbox" do |vbox, override|
+    vbox.gui = true
     vbox.name = "Yocto Developer VM"
     vbox.cpus = 8
     vbox.memory = 8192
@@ -20,6 +21,7 @@ Vagrant.configure("2") do |config|
 
   # vmware specific customizations
   config.vm.provider "vmware_desktop" do |vmware, override|
+    vmware.gui = true
     vmware.vmx["displayname"] = "Yocto Developer VM"
     vmware.vmx["numvcpus"] = "8"
     vmware.vmx["memsize"] = "8192"
@@ -27,6 +29,9 @@ Vagrant.configure("2") do |config|
     vmware.vmx["usb.pcislotnumber"] = "33"
     vmware.vmx["usb_xhci.present"] = "TRUE"
   end
+
+  # make sure the current directory is mounted under /vagrant
+  config.vm.synced_folder ".", "/vagrant", mount_options: ["ro"]
 
   # create new login user
   config.vm.provision "shell", privileged: true, path: 'scripts/setup-vm-user.sh',

Zuehlke/linux-developer-vm-with-ansible

Vagrantfile
@@ -2,14 +2,15 @@
 Vagrant.configure("2") do |config|
 
   # basebox
-  config.vm.box = "tknerr/ubuntu2004-desktop"
-  config.vm.box_version = "0.1.0"
+  config.vm.box = 'generic/ubuntu2004'
+  config.vm.box_version = '3.1.6'
 
   # hostname
   config.vm.hostname = 'dev-vm'
 
   # virtualbox specific customizations
   config.vm.provider "virtualbox" do |vbox, override|
+    vbox.gui = true
     vbox.cpus = 4
     vbox.memory = 4096
     vbox.customize ["modifyvm", :id, "--name", "Linux Developer VM"]
@@ -21,6 +22,7 @@ Vagrant.configure("2") do |config|
 
   # vmware specific customizations
   config.vm.provider "vmware_desktop" do |vmware, override|
+    vmware.gui = true
     vmware.vmx["displayname"] = "Linux Developer VM"
     vmware.vmx["numvcpus"] = "4"
     vmware.vmx["memsize"] = "4096"
@@ -28,6 +30,9 @@ Vagrant.configure("2") do |config|
     vmware.vmx["usb.pcislotnumber"] = "33"
     vmware.vmx["usb_xhci.present"] = "TRUE"
   end
+
+  # make sure the current directory is mounted under /vagrant
+  config.vm.synced_folder ".", "/vagrant", mount_options: ["rw"]
 
   # create new login user
   config.vm.provision "shell", privileged: true, path: 'scripts/setup-vm-user.sh',