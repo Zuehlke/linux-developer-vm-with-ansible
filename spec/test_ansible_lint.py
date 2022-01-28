c9d16511e8d04d873c04d8f1c5a967907bc78fee
Oliviero Chiodo <oliviero.chiodo@zuehlke.com>
2022-01-28 09:12:45 +0100

    Adjust names for ansible-lint version update

--- spec/test_ansible_lint.py
+++ spec/test_ansible_lint.py
@@ -1,11 +1,13 @@
 
-def test_ansible_lint_is_installed_at_version_5_0_12_(host):
+def test_ansible_lint_is_installed_at_version_5_3_2_(host):
     cmd = host.run("pip3 show --disable-pip-version-check ansible-lint")
     assert cmd.rc is 0
     assert "Name: ansible-lint\nVersion: 5.3.2" in cmd.stdout
 
+
 def test_ansible_lint_command_is_found_(host):
     assert host.run('which ansible-lint').rc is 0
 
-def test_ansible_lint_version_command_reports_version_5_0_12_(host):
+
+def test_ansible_lint_version_command_reports_version_5_3_2_(host):
     assert '5.3.2' in host.run('ansible-lint --version').stdout
