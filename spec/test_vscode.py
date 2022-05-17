0a0f628969daf436fc7f1d19880bb1e019bac2e2
Torben Knerr <mail@tknerr.de>
2022-05-17 12:57:18 +0200

    add test to ensure vscode repo is not present in apt sources

--- spec/test_vscode.py
+++ spec/test_vscode.py
@@ -13,3 +13,7 @@ def test_vscode_version_command_reports_version_1_57_1_(host):
 ])
 def test_vscode_extension_is_installed_(host, extension):
     assert extension in host.run('code --list-extensions').stdout
+
+def test_vscode_repo_is_not_present_in_apt_sources_(host):
+    apt_sources = host.run('apt-cache policy').stdout
+    assert 'https://packages.microsoft.com/repos/code' not in apt_sources
