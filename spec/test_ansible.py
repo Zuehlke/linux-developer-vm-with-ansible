5fca226ff5af49fbeb18af602afe87f38b5910a9
Torben Knerr <mail@tknerr.de>
2023-02-27 17:51:10 +0100

    relax ansible-core version check to 2.12.x

--- spec/test_ansible.py
+++ spec/test_ansible.py
@@ -5,10 +5,10 @@ def test_ansible_is_installed_at_version_5_5_0_(host):
     assert "Name: ansible\nVersion: 5.5.0" in cmd.stdout
 
 
-def test_ansible_core_is_installed_at_version_2_12_5_(host):
+def test_ansible_core_is_installed_at_version_2_12_x_(host):
     cmd = host.run("pip3 show --disable-pip-version-check ansible-core")
     assert cmd.rc is 0
-    assert "Name: ansible-core\nVersion: 2.12.5" in cmd.stdout
+    assert "Name: ansible-core\nVersion: 2.12." in cmd.stdout
 
 
 def test_ansible_commands_are_found_(host):
@@ -16,5 +16,5 @@ def test_ansible_commands_are_found_(host):
     assert host.run('which ansible-playbook').rc is 0
 
 
-def test_ansible_version_command_reports_core_version_2_12_5_(host):
-    assert 'core 2.12.5' in host.run('ansible --version').stdout
+def test_ansible_version_command_reports_core_version_2_12_x_(host):
+    assert 'core 2.12.' in host.run('ansible --version').stdout
