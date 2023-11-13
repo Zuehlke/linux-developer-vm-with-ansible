1c0cb4b5bea857e98d3bedef75b05b86c7b9d7c8
Torben Knerr <mail@tknerr.de>
2023-11-13 08:56:51 +0100

    update ansible-lint to latest version 6.22.0

--- spec/test_ansible_lint.py
+++ spec/test_ansible_lint.py
@@ -1,13 +1,13 @@
 
-def test_ansible_lint_is_installed_at_version_5_3_2_(host):
+def test_ansible_lint_is_installed_at_version_6_22_0_(host):
     cmd = host.run("pip3 show --disable-pip-version-check ansible-lint")
     assert cmd.rc is 0
-    assert "Name: ansible-lint\nVersion: 5.3.2" in cmd.stdout
+    assert "Name: ansible-lint\nVersion: 6.22.0" in cmd.stdout
 
 
 def test_ansible_lint_command_is_found_(host):
     assert host.run('which ansible-lint').rc is 0
 
 
-def test_ansible_lint_version_command_reports_version_5_3_2_(host):
-    assert '5.3.2' in host.run('ansible-lint --version').stdout
+def test_ansible_lint_version_command_reports_version_6_22_0_(host):
+    assert '6.22.0' in host.run('ansible-lint --version').stdout
