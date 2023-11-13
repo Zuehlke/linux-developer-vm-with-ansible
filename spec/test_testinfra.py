ef11d8e3e21f10601a123d3b6e3ed499f9d442ac
Torben Knerr <mail@tknerr.de>
2023-11-13 08:53:48 +0100

    update testinfra to latest version 9.0.0

--- spec/test_testinfra.py
+++ spec/test_testinfra.py
@@ -1,8 +1,8 @@
 
-def test_testinfra_is_installed_at_version_6_3_0_(host):
+def test_testinfra_is_installed_at_version_9_0_0_(host):
     cmd = host.run("pip3 show --disable-pip-version-check pytest-testinfra")
     assert cmd.rc is 0
-    assert "Name: pytest-testinfra\nVersion: 6.3.0" in cmd.stdout
+    assert "Name: pytest-testinfra\nVersion: 9.0.0" in cmd.stdout
 
 def test_pytest_spec_is_installed_at_version_3_2_0_(host):
     cmd = host.run("pip3 show --disable-pip-version-check pytest-spec")
